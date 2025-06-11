df4af99867424a692592cf99588e826191645269
Mat Hamilton <mat.hamilton@sedgwick.com>
2025-06-11 15:29:13 -0400

    set validation service to always fail authentication--supposedly

--- Corp.Lib.CertificateAuthentication/Services/AuthenticationService.cs
+++ Corp.Lib.CertificateAuthentication/Services/AuthenticationService.cs
@@ -1,123 +1,125 @@
-﻿using System.Configuration;
+﻿//using System.Configuration;
 using Corp.Lib.CertificateAuthentication.Services.Interfaces;
 using System.Security.Cryptography.X509Certificates;
-using Corp.Lib.CertificateAuthentication.Configuration;
-using Microsoft.Extensions.Options;
+//using Corp.Lib.CertificateAuthentication.Configuration;
+//using Microsoft.Extensions.Options;
 
 namespace Corp.Lib.CertificateAuthentication.Services
 {
-	public class AuthenticationService(IOptions<ClientCertificateSettings> options) : IAuthenticationService
+	public class AuthenticationService/*(IOptions<ClientCertificateSettings> options)*/ : IAuthenticationService
 	{
-		private readonly ClientCertificateSettings _ClientCertificateSettings = options.Value;
+		//private readonly ClientCertificateSettings _ClientCertificateSettings = options.Value;
 
 		public bool IsValidClientCertificate(X509Certificate2 clientCertificate)
 		{
-			if (clientCertificate == null!)
-			{
-				var error = new ConfigurationErrorsException("Missing certificate or certificate not sent by client.");
+			return false;
 
-				Corp.Lib.Logging.Logger.Log.Error(error, "Certificate validation failed. Missing certificate or certificate not sent by client.");
+			//if (clientCertificate == null!)
+			//{
+			//	var error = new ConfigurationErrorsException("Missing certificate or certificate not sent by client.");
 
-				throw error;
-			}
+			//	//Corp.Lib.Logging.Logger.Log.Error(error, "Certificate validation failed. Missing certificate or certificate not sent by client.");
 
-			if (_ClientCertificateSettings == null! || _ClientCertificateSettings.AllowedCertificates == null! || !_ClientCertificateSettings.AllowedCertificates.ToList().Any())
-			{
-				var error = new ConfigurationErrorsException("Certificate configuration missing. Check AppSettings.");
+			//	throw error;
+			//}
 
-				Corp.Lib.Logging.Logger.Log.Error(error, "Certificate validation failed. Certificate configuration missing. Check AppSettings.");
+			//if (_ClientCertificateSettings == null! || _ClientCertificateSettings.AllowedCertificates == null! || !_ClientCertificateSettings.AllowedCertificates.ToList().Any())
+			//{
+			//	var error = new ConfigurationErrorsException("Certificate configuration missing. Check AppSettings.");
 
-				throw error;
-			}
+			//	//Corp.Lib.Logging.Logger.Log.Error(error, "Certificate validation failed. Certificate configuration missing. Check AppSettings.");
 
-			if (_ClientCertificateSettings.AllowedCertificates.Any(cert => string.IsNullOrEmpty(cert.Subject)))
-			{
-				var error = new ConfigurationErrorsException("Certificate configuration missing Subject. Check AppSettings.");
+			//	throw error;
+			//}
 
-				Corp.Lib.Logging.Logger.Log.Error(error, "Certificate validation failed. Certificate configuration missing Subject. Check AppSettings.");
+			//if (_ClientCertificateSettings.AllowedCertificates.Any(cert => string.IsNullOrEmpty(cert.Subject)))
+			//{
+			//	var error = new ConfigurationErrorsException("Certificate configuration missing Subject. Check AppSettings.");
 
-				throw error;
-			}
+			//	//Corp.Lib.Logging.Logger.Log.Error(error, "Certificate validation failed. Certificate configuration missing Subject. Check AppSettings.");
 
-			if (_ClientCertificateSettings.AllowedCertificates.Any(cert => string.IsNullOrEmpty(cert.Issuer)))
-			{
-				var error = new ConfigurationErrorsException("Certificate configuration missing Subject. Check AppSettings.");
+			//	throw error;
+			//}
 
-				Corp.Lib.Logging.Logger.Log.Error(error, "Certificate validation failed. Certificate configuration missing Issuer. Check AppSettings.");
+			//if (_ClientCertificateSettings.AllowedCertificates.Any(cert => string.IsNullOrEmpty(cert.Issuer)))
+			//{
+			//	var error = new ConfigurationErrorsException("Certificate configuration missing Subject. Check AppSettings.");
 
-				throw error;
-			}
+			//	//Corp.Lib.Logging.Logger.Log.Error(error, "Certificate validation failed. Certificate configuration missing Issuer. Check AppSettings.");
 
-			// 1. Check time validity of certificate.
-			if (DateTime.Compare(DateTime.UtcNow, clientCertificate.NotBefore) < 0 || DateTime.Compare(DateTime.UtcNow, clientCertificate.NotAfter) > 0)
-			{
-				Corp.Lib.Logging.Logger.Log.Warning($"Certificate with thumbprint {clientCertificate.Thumbprint} is expired.");
+			//	throw error;
+			//}
 
-				return false;
-			}
+			//// 1. Check time validity of certificate.
+			//if (DateTime.Compare(DateTime.UtcNow, clientCertificate.NotBefore) < 0 || DateTime.Compare(DateTime.UtcNow, clientCertificate.NotAfter) > 0)
+			//{
+			//	//Corp.Lib.Logging.Logger.Log.Warning($"Certificate with thumbprint {clientCertificate.Thumbprint} is expired.");
 
-			// 2. Check subject name of certificate.
-			var foundSubject = false;
+			//	return false;
+			//}
 
-			var certSubjectData = clientCertificate.Subject.Split([','], StringSplitOptions.RemoveEmptyEntries);
+			//// 2. Check subject name of certificate.
+			//var foundSubject = false;
 
-			if (certSubjectData.Any(certSubject => _ClientCertificateSettings.AllowedCertificates.Any(cert => cert.Subject.Equals(certSubject.Trim(), StringComparison.InvariantCultureIgnoreCase))))
-			{
-				foundSubject = true;
-			}
+			//var certSubjectData = clientCertificate.Subject.Split([','], StringSplitOptions.RemoveEmptyEntries);
 
-			if (!foundSubject)
-			{
-				Corp.Lib.Logging.Logger.Log.Warning($"Certificate with thumbprint {clientCertificate.Thumbprint} does not have a matching Subject.");
+			//if (certSubjectData.Any(certSubject => _ClientCertificateSettings.AllowedCertificates.Any(cert => cert.Subject.Equals(certSubject.Trim(), StringComparison.InvariantCultureIgnoreCase))))
+			//{
+			//	foundSubject = true;
+			//}
 
-				return false;
-			}
+			//if (!foundSubject)
+			//{
+			//	//Corp.Lib.Logging.Logger.Log.Warning($"Certificate with thumbprint {clientCertificate.Thumbprint} does not have a matching Subject.");
 
-			// 3. Check issuer name of certificate.
-			var certIssuerData = clientCertificate.Issuer.Split([','], StringSplitOptions.RemoveEmptyEntries);
+			//	return false;
+			//}
 
-			var foundIssuer = certIssuerData.Any(issuerData => _ClientCertificateSettings.AllowedCertificates.Any(cert => cert.Issuer.Equals(issuerData.Trim(), StringComparison.InvariantCultureIgnoreCase)));
+			//// 3. Check issuer name of certificate.
+			//var certIssuerData = clientCertificate.Issuer.Split([','], StringSplitOptions.RemoveEmptyEntries);
 
-			if (!foundIssuer)
-			{
-				Corp.Lib.Logging.Logger.Log.Warning($"Certificate with thumbprint {clientCertificate.Thumbprint} does not have a matching Issuer.");
+			//var foundIssuer = certIssuerData.Any(issuerData => _ClientCertificateSettings.AllowedCertificates.Any(cert => cert.Issuer.Equals(issuerData.Trim(), StringComparison.InvariantCultureIgnoreCase)));
 
-				return false;
-			}
+			//if (!foundIssuer)
+			//{
+			//	//Corp.Lib.Logging.Logger.Log.Warning($"Certificate with thumbprint {clientCertificate.Thumbprint} does not have a matching Issuer.");
 
-			// Check if the Certificate exists in the server personal cert store.
-			if (_ClientCertificateSettings.CheckThumbprintInCertStore)
-			{
-				var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+			//	return false;
+			//}
 
-				try
-				{
-					store.Open(OpenFlags.OpenExistingOnly | OpenFlags.ReadOnly);
+			//// Check if the Certificate exists in the server personal cert store.
+			//if (_ClientCertificateSettings.CheckThumbprintInCertStore)
+			//{
+			//	var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
 
-					var certs = store.Certificates.Find(X509FindType.FindByThumbprint, clientCertificate.Thumbprint, true);
+			//	try
+			//	{
+			//		store.Open(OpenFlags.OpenExistingOnly | OpenFlags.ReadOnly);
 
-					if (certs.Count == 0)
-					{
-						Corp.Lib.Logging.Logger.Log.Warning("Invalid client certificate. The thumbprint does not match with a certificate in the certificate store. {@ClientCertificate}", clientCertificate);
+			//		var certs = store.Certificates.Find(X509FindType.FindByThumbprint, clientCertificate.Thumbprint, true);
 
-						return false;
-					}
-				}
-				catch (Exception ex)
-				{
-					Corp.Lib.Logging.Logger.Log.Error(ex, "An exception occurred searching for the client certificate in the certificate store. {@ClientCertificate}", clientCertificate);
+			//		if (certs.Count == 0)
+			//		{
+			//			//Corp.Lib.Logging.Logger.Log.Warning("Invalid client certificate. The thumbprint does not match with a certificate in the certificate store. {@ClientCertificate}", clientCertificate);
 
-					throw;
-				}
-				finally
-				{
-					store.Close();
+			//			return false;
+			//		}
+			//	}
+			//	//catch (Exception ex)
+			//	//{
+			//	//	Corp.Lib.Logging.Logger.Log.Error(ex, "An exception occurred searching for the client certificate in the certificate store. {@ClientCertificate}", clientCertificate);
 
-					store.Dispose();
-				}
-			}
+			//	//	throw;
+			//	//}
+			//	finally
+			//	{
+			//		store.Close();
 
-			return true;
+			//		store.Dispose();
+			//	}
+			//}
+
+			//return true;
 		}
 	}
 }
